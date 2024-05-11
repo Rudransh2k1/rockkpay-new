fe62490bf5f60e154229dff30d7edb1186f5e5f6
W2-80459 <144530473+W2-80459@users.noreply.github.com>
2024-05-11 15:49:31 +0530

    navbar added

--- src/common/Navbar.jsx
+++ src/common/Navbar.jsx
@@ -10,31 +10,32 @@ import "./navbar.css";
 const Navbar = () => {
   const data = {
     heading: "Hello World",
-    message: "Welcome to our website!",
+    message: "Hello Retailer",
   };
 
   return (
     <AppBar position="relative" color="primary">
-      <Toolbar className="Toolbar-root">
-  <div className="w-full flex items-center">
-    <Typography variant="h6" component="div" className="Typography-root">
-      {data.heading}
-    </Typography>
-  </div>
-  <div className="flex items-center gap-6 end-icons">
-  <Typography variant="body1">
-    {data.message}
-  </Typography>
-  <div className="Divider-root"></div>
-  <IconButton color="inherit" aria-label="notifications" className="IconButton-root">
-    <NotificationsIcon />
-  </IconButton>
-  <IconButton color="inherit" aria-label="account" className="IconButton-root">
-    <AccountCircleIcon />
-  </IconButton>
-</div>
-</Toolbar>
-    </AppBar>
+    <Toolbar className="Toolbar-root">
+      <div className="w-full flex items-center">
+        <Typography variant="h6" component="div" className="Typography-root">
+          {data.heading}
+        </Typography>
+      </div>
+      <div className="flex items-center gap-6 end-icons">
+        <Typography variant="body1" className="message">
+          {data.message}
+        </Typography>
+        <div className="Divider-root"></div>
+        <IconButton color="inherit" aria-label="notifications" className="IconButton-root">
+          <NotificationsIcon />
+        </IconButton>
+        <IconButton color="inherit" aria-label="account" className="IconButton-root">
+          <AccountCircleIcon />
+        </IconButton>
+      </div>
+    </Toolbar>
+  </AppBar>
+
   );
 };
 
